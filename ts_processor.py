87a16e82dc66bf9777b8f1014237e97e29c25ea9
taneljairus <tanel@jairus.ee>
2021-03-13 09:05:25 +0200

    Improvements in logging, conf
    Interpolation is now configurable.
    Console output reworked, warnings hidden.

--- ts_processor.py
+++ ts_processor.py
@@ -12,12 +12,14 @@ import os
 import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', required = True, type=str)
-parser.add_argument('--sampling_interval', default = '0.5', type=float)
-parser.add_argument('--folder', default = 'output', type=str)
-parser.add_argument('--timeshift', default = '0', type=float)
-parser.add_argument('--min_speed', default = '-1', type=float)
+parser.add_argument('--input', required = True, type=str) #input file or folder
+parser.add_argument('--sampling_interval', default = '0.5', type=float) #distance between images in seconds.
+parser.add_argument('--folder', default = 'output', type=str) #output folder, will be created if not exists
+parser.add_argument('--timeshift', default = '0', type=float) #time shift in seconds, if the gps and video seem out of sync
+parser.add_argument('--min_speed', default = '-1', type=float) #minimum speed in m/s to filter out stops
 parser.add_argument('--bearing_modifier', default = '0', type=float) #180 if rear camera
+parser.add_argument('--min_coverage', default = '90', type=int) #percentage - how much video must have GPS data in order to interpolate missing
+parser.add_argument('--min_points', default = '5', type=int) #how many points to allow video extraction
 args = parser.parse_args()
 print(args)
 input_ts_file = args.input
@@ -25,7 +27,35 @@ folder = args.folder
 timeshift = args.timeshift
 
 
+# Define a context manager to suppress stdout and stderr.
+class suppress_stdout_stderr(object): #from here: https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions
+    '''
+    A context manager for doing a "deep suppression" of stdout and stderr in 
+    Python, i.e. will suppress all print, even if the print originates in a 
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).      
 
+    '''
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds =  [os.open(os.devnull,os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = [os.dup(1), os.dup(2)]
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0],1)
+        os.dup2(self.null_fds[1],2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0],1)
+        os.dup2(self.save_fds[1],2)
+        # Close all file descriptors
+        for fd in self.null_fds + self.save_fds:
+            os.close(fd)
 
 def fix_coordinates(hemisphere,coordinate_input): #From here: https://sergei.nz/extracting-gps-data-from-viofo-a119-and-other-novatek-powered-cameras/
     coordinate, = coordinate_input
@@ -71,7 +101,7 @@ for input_ts_file in inputfiles:
     interval = int(args.sampling_interval*fps)
     make = "unknown"
     model = "unknown"
-    packetno = 1
+    packetno = 0
     locdata = {}
     prevpacket = None
     with open(input_ts_file, "rb") as f:
@@ -159,115 +189,122 @@ for input_ts_file in inputfiles:
             prevpacket = input_packet
             del currentdata
     print ("GPS data analysis ended, no of points ", len(locdata))
-    if len(locdata)<length/fps:
-        print ("Missing GPS points. Interpolating")
-        i = 0
-        while i < length/fps:
-            if i not in locdata:
-                #Find previous existing
-                prev_data = i - 1
-                next_data = i + 1
-                while prev_data not in locdata and prev_data>0:
-                    prev_data -= 1
-             
-                #Find next existing
-                while locdata[next_data] is None and next<length/fps:
-                    next_data += 1
-                if prev_data in locdata and next_data in locdata:
-                    currentdata = {}
-
-                    current_position = float(i-prev_data)/float(next_data-prev_data)
-                    currentdata["ts"] = locdata[prev_data]["ts"]+(locdata[(next_data)]["ts"]-locdata[prev_data]["ts"])*current_position
-                    currentdata["lat"] = locdata[prev_data]["lat"]+(locdata[(next_data)]["lat"]-locdata[prev_data]["lat"])*current_position
-                    currentdata["lon"] = locdata[prev_data]["lon"]+(locdata[(next_data)]["lon"]-locdata[prev_data]["lon"])*current_position
-                    currentdata["bearing"] = locdata[prev_data]["bearing"]+(locdata[(next_data)]["bearing"]-locdata[prev_data]["bearing"])*current_position
-                    currentdata["speed"] = locdata[prev_data]["speed"]+(locdata[(next_data)]["speed"]-locdata[prev_data]["speed"])*current_position
-                    locdata[i] = currentdata
-                    del currentdata
-            i=i+1
-    i=0
-    while not i in locdata:
-        i+=1  #extrapolate down
-    
-    while i > -10:
-        if not i in locdata:
-            currentdata = {}
-            
-            currentdata["ts"] = locdata[i+1]["ts"]-(locdata[(i+2)]["ts"]-locdata[i+1]["ts"])
-            currentdata["lat"] = locdata[i+1]["lat"]-(locdata[(i+2)]["lat"]-locdata[i+1]["lat"])
-            currentdata["lon"] = locdata[i+1]["lon"]-(locdata[(i+2)]["lon"]-locdata[i+1]["lon"])
-            currentdata["bearing"] = locdata[i+1]["bearing"]-(locdata[(i+2)]["bearing"]-locdata[i+1]["bearing"])
-            currentdata["speed"] = locdata[i+1]["speed"]-(locdata[(i+2)]["speed"]-locdata[i+1]["speed"])
-            locdata[i] = currentdata
-            del currentdata
-        i-=1
-    i=0
-    while i in locdata:
-        i+=1
-    while i < length / fps * 1.5:
-        if not i in locdata:
-            currentdata = {}
-            
-            currentdata["ts"] = locdata[i-1]["ts"]-(locdata[(i-2)]["ts"]-locdata[i-1]["ts"])
-            currentdata["lat"] = locdata[i-1]["lat"]-(locdata[(i-2)]["lat"]-locdata[i-1]["lat"])
-            currentdata["lon"] = locdata[i-1]["lon"]-(locdata[(i-2)]["lon"]-locdata[i-1]["lon"])
-            currentdata["bearing"] = locdata[i-1]["bearing"]-(locdata[(i-2)]["bearing"]-locdata[i-1]["bearing"])
-            currentdata["speed"] = locdata[i-1]["speed"]-(locdata[(i-2)]["speed"]-locdata[i-1]["speed"])
-            locdata[i] = currentdata
-            del currentdata
-        i+=1
-    if len(locdata)<5:
-        print ("No GPS data")
+    if len(locdata)<args.min_coverage*length*0.01/fps:
+        print ("Not enough GPS data for interpolation",args.min_coverage,"% needed, ",len(locdata)*100/length*fps,"% found")
     else:
-        print ("Video extraction started")
-        framecount = 0
-        count = 0
-        success,image = video.read()
-        while success:
-            if framecount % interval == 0:
+        if len(locdata)<length/fps:
+            print ("Interpolating missing points")
+            i = 0
+            while i < length/fps:
+                if i not in locdata:
+                    #Find previous existing
+                    prev_data = i - 1
+                    next_data = i + 1
+                    while prev_data not in locdata and prev_data>0:
+                        prev_data -= 1
+                 
+                    #Find next existing
+                    while locdata[next_data] is None and next<length/fps:
+                        next_data += 1
+                    if prev_data in locdata and next_data in locdata:
+                        currentdata = {}
+
+                        current_position = float(i-prev_data)/float(next_data-prev_data)
+                        currentdata["ts"] = locdata[prev_data]["ts"]+(locdata[(next_data)]["ts"]-locdata[prev_data]["ts"])*current_position
+                        currentdata["lat"] = locdata[prev_data]["lat"]+(locdata[(next_data)]["lat"]-locdata[prev_data]["lat"])*current_position
+                        currentdata["lon"] = locdata[prev_data]["lon"]+(locdata[(next_data)]["lon"]-locdata[prev_data]["lon"])*current_position
+                        currentdata["bearing"] = locdata[prev_data]["bearing"]+(locdata[(next_data)]["bearing"]-locdata[prev_data]["bearing"])*current_position
+                        currentdata["speed"] = locdata[prev_data]["speed"]+(locdata[(next_data)]["speed"]-locdata[prev_data]["speed"])*current_position
+                        locdata[i] = currentdata
+                        del currentdata
+                i=i+1
+        i=0
+        while not i in locdata:
+            i+=1  #extrapolate down
+        
+        while i > -10:
+            if not i in locdata:
+                currentdata = {}
                 
-                try:
-                    #interpolate time and coordinates
-                    prev_dataframe = (float(math.trunc((framecount+timeshift*fps)/fps)))
-                    current_position = (framecount + timeshift*fps - prev_dataframe*fps)/fps 
-                    new_speed = locdata[prev_dataframe]["speed"]+(locdata[(prev_dataframe+1)]["speed"]-locdata[prev_dataframe]["speed"])*current_position
-                    if new_speed >= args.min_speed:
-                        
-                        new_ts = locdata[prev_dataframe]["ts"]+(locdata[(prev_dataframe+1)]["ts"]-locdata[prev_dataframe]["ts"])*current_position
-                        new_lat = locdata[prev_dataframe]["lat"]+(locdata[(prev_dataframe+1)]["lat"]-locdata[prev_dataframe]["lat"])*current_position
-                        new_lon = locdata[prev_dataframe]["lon"]+(locdata[(prev_dataframe+1)]["lon"]-locdata[prev_dataframe]["lon"])*current_position
-                        new_bear = args.bearing_modifier + locdata[prev_dataframe]["bearing"]+(locdata[(prev_dataframe+1)]["bearing"]-locdata[prev_dataframe]["bearing"])*current_position
-                        while new_bear < 0:
-                            new_bear += 360
-                        while new_bear > 360:
-                            new_bear -= 360
-                        lonref, lon2 = to_gps_latlon(new_lon, ('E', 'W'))
-                        latref, lat2 = to_gps_latlon(new_lat, ('N', 'S'))
-                        cv2.imwrite("tmp.jpg", image)
-                        e_image = Image("tmp.jpg")
-                        e_image.gps_latitude = lat2
-                        e_image.gps_latitude_ref = latref
-                        e_image.gps_longitude  = lon2
-                        e_image.gps_longitude_ref = lonref
-                        e_image.gps_img_direction = new_bear
-                        e_image.gps_dest_bearing = new_bear
-                        e_image.make = make
-                        e_image.model = model
-                        datetime_taken = datetime.fromtimestamp(new_ts)
-                        e_image.datetime_original = datetime_taken.strftime(DATETIME_STR_FORMAT)
-                        
-                        with open(folder+os.path.sep+input_ts_file.replace(".ts","_") + "_"+"%06d" % count + ".jpg", 'wb') as new_image_file:
-                            new_image_file.write(e_image.get_file())
-                        #print('Frame: ', framecount)
-                        count += 1
-                except:
+                currentdata["ts"] = locdata[i+1]["ts"]-(locdata[(i+2)]["ts"]-locdata[i+1]["ts"])
+                currentdata["lat"] = locdata[i+1]["lat"]-(locdata[(i+2)]["lat"]-locdata[i+1]["lat"])
+                currentdata["lon"] = locdata[i+1]["lon"]-(locdata[(i+2)]["lon"]-locdata[i+1]["lon"])
+                currentdata["bearing"] = locdata[i+1]["bearing"]-(locdata[(i+2)]["bearing"]-locdata[i+1]["bearing"])
+                currentdata["speed"] = locdata[i+1]["speed"]-(locdata[(i+2)]["speed"]-locdata[i+1]["speed"])
+                locdata[i] = currentdata
+                del currentdata
+            i-=1
+        i=0
+        while i in locdata:
+            i+=1
+        while i < length / fps * 1.5:
+            if not i in locdata:
+                currentdata = {}
                 
-                    print ("Error processing frame %d, skipped." % framecount)
+                currentdata["ts"] = locdata[i-1]["ts"]-(locdata[(i-2)]["ts"]-locdata[i-1]["ts"])
+                currentdata["lat"] = locdata[i-1]["lat"]-(locdata[(i-2)]["lat"]-locdata[i-1]["lat"])
+                currentdata["lon"] = locdata[i-1]["lon"]-(locdata[(i-2)]["lon"]-locdata[i-1]["lon"])
+                currentdata["bearing"] = locdata[i-1]["bearing"]-(locdata[(i-2)]["bearing"]-locdata[i-1]["bearing"])
+                currentdata["speed"] = locdata[i-1]["speed"]-(locdata[(i-2)]["speed"]-locdata[i-1]["speed"])
+                locdata[i] = currentdata
+                del currentdata
+            i+=1
+if len(locdata)<args.min_points:
+    print ("Not enough GPS data for frame extraction.")
+else:
+    print ("Video extraction started")
+    framecount = 0
+    count = 0
+    success,image = video.read()
+    while success:
+        if framecount % interval == 0:
             
-            framecount += int(fps*args.sampling_interval)
-            #print('Frame: ', framecount)
+            try:
+                #interpolate time and coordinates
+                prev_dataframe = (float(math.trunc((framecount+timeshift*fps)/fps)))
+                current_position = (framecount + timeshift*fps - prev_dataframe*fps)/fps 
+                new_speed = locdata[prev_dataframe]["speed"]+(locdata[(prev_dataframe+1)]["speed"]-locdata[prev_dataframe]["speed"])*current_position
+                if new_speed >= args.min_speed:
+                    
+                    new_ts = locdata[prev_dataframe]["ts"]+(locdata[(prev_dataframe+1)]["ts"]-locdata[prev_dataframe]["ts"])*current_position
+                    new_lat = locdata[prev_dataframe]["lat"]+(locdata[(prev_dataframe+1)]["lat"]-locdata[prev_dataframe]["lat"])*current_position
+                    new_lon = locdata[prev_dataframe]["lon"]+(locdata[(prev_dataframe+1)]["lon"]-locdata[prev_dataframe]["lon"])*current_position
+                    new_bear = args.bearing_modifier + locdata[prev_dataframe]["bearing"]+(locdata[(prev_dataframe+1)]["bearing"]-locdata[prev_dataframe]["bearing"])*current_position
+                    while new_bear < 0:
+                        new_bear += 360
+                    while new_bear > 360:
+                        new_bear -= 360
+                    lonref, lon2 = to_gps_latlon(new_lon, ('E', 'W'))
+                    latref, lat2 = to_gps_latlon(new_lat, ('N', 'S'))
+                    cv2.imwrite("tmp.jpg", image)
+                    e_image = Image("tmp.jpg")
+                    e_image.gps_latitude = lat2
+                    e_image.gps_latitude_ref = latref
+                    e_image.gps_longitude  = lon2
+                    e_image.gps_longitude_ref = lonref
+                    e_image.gps_img_direction = new_bear
+                    e_image.gps_dest_bearing = new_bear
+                    e_image.make = make
+                    e_image.model = model
+                    datetime_taken = datetime.fromtimestamp(new_ts)
+                    e_image.datetime_original = datetime_taken.strftime(DATETIME_STR_FORMAT)
+                    
+                    with open(folder+os.path.sep+input_ts_file.replace(".ts","_") + "_"+"%06d" % count + ".jpg", 'wb') as new_image_file:
+                        new_image_file.write(e_image.get_file())
+                    #print('Frame: ', framecount)
+                    count += 1
+            except:
+            
+                print ("Error processing frame %d, skipped." % framecount)
+        
+        framecount += int(fps*args.sampling_interval)
+        #print('Frame: ', framecount)
+        with suppress_stdout_stderr(): #Just to keep the console clear from OpenCV warning messages
             video.set(1,framecount)
-            success,image = video.read()
-        video.release()
+        success,image = video.read()
+    video.release()
+    try:
         os.unlink("tmp.jpg")
-        print (input_ts_file, " processed")
+    except:
+        pass
+    print (input_ts_file, " processed, ", count, " images extracted")
